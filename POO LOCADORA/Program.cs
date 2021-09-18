440142dfeeb8a1e0a4465d12f13a95f41d07fdde
Isaías De Jesus Barros <88753616+IsaiasBrrsJ@users.noreply.github.com>
2021-09-18 18:35:52 -0300

    C# POO Locadora
    Desenvolver uma sistema de locadora onde calcula o seguro pago do cliente , obedecendo os seguintes critérios:
    Valor do seguro:
    Carro Popular 10%
    Carro de Luxo 20%
    Carro utilitário 25%
    
    Valor das diárias :
    Carro Popular R$ 100,00
    Carro de Luxo R$ 300,00
    Carro utilitário R$ 400,00
    
    Obs: a porcentagem é em cima do valor utilizado

---
+++ POO LOCADORA/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace ProjetoLocadora
+{
+    class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            ProgramMenu();
+        }
+
+
+        enum Menu { CarroPopular = 1, CarroDeLuxo, CarroUtilitario, Sair }
+        static void ProgramMenu()
+        {
+
+           
+
+            bool EscolheuSair = !true;
+
+            while (EscolheuSair != true)
+            {
+               
+                Console.WriteLine("\n\t   LOCADORA DE VEÍCULOS  === Bart ===");
+                Console.WriteLine("\n\t\t1- Carro Popular\n\t\t2- Carro de Luxo\n\t\t3- Carro Utilitário\n\t\t4-Sair");
+                Console.Write("\n\t\tEscolha uma opção:  ");
+                int opc = int.Parse(Console.ReadLine());
+
+                Menu opEscolhida = (Menu)opc;
+
+                if (opc > 0 && opc <= 4)
+                {
+
+                    switch (opEscolhida)
+                    {
+                        case Menu.CarroPopular:
+
+                            SeguroDiarias CarPopular = new CarroPopular();
+                            Console.WriteLine($"\n\t\t{opEscolhida} ");
+                            Console.Write("\n\t\tInforme quantidade de diarias: ");
+                            CarPopular.CalcSeg(int.Parse(Console.ReadLine()));
+
+                            break;
+
+                        case Menu.CarroDeLuxo:
+
+                            SeguroDiarias carLuxo = new CarroDeLuxo();
+                            Console.WriteLine($"\n\t\t{opEscolhida} ");
+                            Console.Write("\n\t\tInforme quantidade de diarias: ");
+                            carLuxo.CalcSeg(int.Parse(Console.ReadLine()));
+
+                            break;
+
+                        case Menu.CarroUtilitario:
+
+                            SeguroDiarias carUtilitario = new CarroUtilitario();
+                            Console.WriteLine($"\n\t\t{opEscolhida} ");
+                            Console.Write("\n\t\tInforme quantidade de diarias: ");
+                            carUtilitario.CalcSeg(int.Parse(Console.ReadLine()));
+
+                            break;
+                        case Menu.Sair:
+                            EscolheuSair = !false;
+                            Console.WriteLine($"\n\t\tATÉ BREVE.....");
+                            Thread.Sleep(1000);
+                            Console.Beep();
+                            Console.Clear();
+                            break;
+
+                        default:
+                            Console.WriteLine("\n\t\tEscolha Entre 1 e 3\n\t\tPressione para continuar..");
+                            Console.ReadKey();
+                            Console.Clear();
+                            ProgramMenu();
+                            break;
+                    }
+
+                }
+
+                if (EscolheuSair != true)
+                {
+                    Console.ReadKey();
+                }
+
+                Console.Clear();
+            }
+    
+            
+        }
+    }
+}
---
+++ POO LOCADORA/SeguroDiarias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLocadora
+{
+    class SeguroDiarias
+    {
+       
+        public virtual void CalcSeg(int dias)
+        {
+            
+            Console.WriteLine($"\nValor da diária: \nQuantidade De Diarias: {dias}\nValor do seguro: ");
+
+        }
+
+
+    }
+}
